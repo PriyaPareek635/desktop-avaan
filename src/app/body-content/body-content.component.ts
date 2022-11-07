8293c02da142ae84f98038ed2163c147abf433c4
Priya Pareek <priya.pareek@olacabs.com>
2022-11-08 00:42:22 +0530

    added: body components

---
+++ src/app/body-content/body-content.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-content',
+  templateUrl: './body-content.component.html',
+  styleUrls: ['./body-content.component.css']
+})
+export class BodyContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
